dc0d6d6d66a8df6869f750c80c89c97233395c2d
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-06-26 19:31:50 -0400

    get request complete

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      smurfs: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then (res => {
+        console.log(res)
+        const smurfData = res.data
+        this.setState({smurfs: smurfData});
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
   render() {
     return (
       <div className="App">
